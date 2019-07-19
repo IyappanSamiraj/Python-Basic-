c6d22a86b261834ca0ad598291ba44738d9c2c01
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2019-07-19 15:24:07 +0530

    Update Bs3f1.py

--- Bs3f1.py
+++ Bs3f1.py
@@ -2,6 +2,6 @@ var=str(input())
 if (var=='a' or var=='A' or var=='e' or var=='E' or var=='i' or var=='I' or var=='o' or var=='O' or var=='u' or var=='U'):
     print('Vowel')
 elif (var>='a' and var<='z') or (var>='A' and var<='Z'):
-    print('consonant')
+    print('Consonant')
 else:
-    print('Invalid')
+    print('invalid')
