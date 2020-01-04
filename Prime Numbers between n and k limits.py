35ec8843893f5da32b16201ccd7dcb144c1ee5f3
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2020-01-04 10:59:17 +0530

    Update and rename Bs16f2.py to Prime Numbers between n and k limits.py

--- Prime Numbers between n and k limits.py
+++ Prime Numbers between n and k limits.py
@@ -1,5 +1,5 @@
-num1,num2=map(int,input().split())
-for i in range(num1+1, num2):
+n,k=map(int,input().split())
+for i in range(n+1, k):
     if i>1:
         for num in range(2,i):
             if (i % num) == 0:
