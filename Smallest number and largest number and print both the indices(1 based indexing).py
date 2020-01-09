00c527109d2cf4df9b0fae50502b566198214fc8
Iyappan Samiraj <52095660+IyappanSamiraj@users.noreply.github.com>
2020-01-09 01:31:33 -0800

    Create  Smallest number and largest number and print both the indices(1 based indexing).py

---
+++ Smallest number and largest number and print both the indices(1 based indexing).py
@@ -0,0 +1,12 @@
+'''Given a number N followed by N numbers.Find the smallest number and largest number and print both the indices(1 based indexing).
+Input Size : N <= 100000
+Sample Testcase :
+INPUT
+5
+1 2 3 4 5
+OUTPUT
+1 '''
+
+n=int(input())
+lst=list(map(int,input().split()))
+print(lst.index(min(lst))+1,lst.index(max(lst))+1)
