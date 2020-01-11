ae0f6627ae849f71ba1e9849cd7be229794ed92e
Iyappan Samiraj <52095660+IyappanSamiraj@users.noreply.github.com>
2020-01-11 21:35:10 +0530

    Update Given Number is Odd or Even.py

--- Given Number is Odd or Even.py
+++ Given Number is Odd or Even.py
@@ -1,7 +1,14 @@
+#get the input
 num=int(input())
+
+#Condition is satisfied only the number is divisible by 2
 if num%2==0:
     print('Even')
+
+#If else condition Is satisfied at if condition not satisfied 
 elif num<0:
     print('Invalid')
+
+#In if and elif Condition not satisfied Default else printed
 else:
     print('Odd')
